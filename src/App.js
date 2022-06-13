c94b7fcc9293f7e6e2e065ee412d7b277bbf2f6c
kairos67 <kairosys@gmail.com>
2022-06-13 19:34:42 +0900

    04 리액트 훅스 useState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import React, {useState} from 'react';
 
 function App() {
+  const [text, setText] = useState('Hello, world');
+
+  const onSubmit = ()=>{
+    alert('onSubmited');
+  };
+  const onKeyUp = (event)=>{
+    if(event.keyCode ===13){
+      onSubmit();
+    }
+    console.log('onKeyUp');
+  }
+
+  //let text = 'Hello';
+  const updateText = ()=>{
+    //text = 'Kairos';
+    setText('Kairos');
+  }
   return (
     <div className="App">
-      Kairos Coder
+      <h1>Kairos Coder</h1>
+            <input onKeyUp={onKeyUp} onSubmit={onSubmit}/>
+      <button onClick={onSubmit}>Submit</button>
+      <br /> <br /> 
+
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>  
     </div>
   );
 }
