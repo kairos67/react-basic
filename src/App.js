425c7dcf05caf31831939e2f5c23a7383322c807
kairos67 <kairosys@gmail.com>
2022-06-28 18:55:38 +0900

    add component movie forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Movie from './components/Movie';
+import MovieForm from './components/MovieForms';
 
 function App() {
-  const [movieTitle, setMovieTitle] = useState('');
-  const [movieYear, setMovieYear] = useState('');
+  // const [movieTitle, setMovieTitle] = useState('');
+  // const [movieYear, setMovieYear] = useState('');
   const [movies, setMovies] = useState([
     { title: 'kairos 1', year: 2001 },
     { title: 'kairos 2', year: 2002 },
@@ -19,36 +20,18 @@ function App() {
       <Movie movie={movie} key={movie.title} />
     );
   });
-  const addMovie = (event) => {
-    event.preventDefault();
-    // console.log(movieTitle,movieYear);
+  const addMovie = (movie) => {
+        // console.log(movieTitle,movieYear);
     setMovies([
       ...movies, //기존값을 유지
-      { 
-      title: movieTitle, 
-      year: movieYear 
-    }]);
-    setMovieTitle('');
-    setMovieYear('');
+      movie  
+    ]);    
   };
+
   return (
     <div className="App">
       <h1>Movie list</h1>
-      <form onSubmit={addMovie}>
-        <input
-          type="text"
-          value={movieTitle}
-          placeholder='영화제목'
-          onChange={e => setMovieTitle(e.target.value)}
-        /> <br />
-        <input
-          type="text"
-          value={movieYear}
-          placeholder='개봉년도'
-          onChange={e => setMovieYear(e.target.value)}
-        /> <br />
-        <button type='submit'>영화추가</button>
-      </form>
+      < MovieForm addMovie={addMovie}/>
       {renderMovies}
     </div>
   );
